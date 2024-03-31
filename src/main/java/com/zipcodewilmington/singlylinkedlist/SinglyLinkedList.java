c6eaf09b182cb05a217a256ea3689c099a64be67
asanahmadli <asenahmedli@gmail.com>
2024-03-30 21:34:04 -0400

    I added test cases and code

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,108 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+
+    List<T> list = new ArrayList<>();
+
+    public void add(Integer value) {
+        list.add((T) value);
+    }
+
+    public boolean contains(Integer value) {
+        return list.contains(value);
+    }
+
+    public void remove(Integer value) {
+        list.remove(value);
+    }
+
+    public Integer find(Integer value) {
+        if(list.contains(value)){
+            return list.indexOf(value);
+        }
+        else {
+            return -1;
+        }
+    }
+
+    public Integer size() {
+        return list.size();
+    }
+
+    public Integer get(Integer value) {
+        return (Integer) list.get(value);
+    }
+
+    public List<Integer> sort(SinglyLinkedList singlyLinkedList) {
+        return singlyLinkedList.sort(singlyLinkedList);
+    }
+
+    public List<Integer> copy(SinglyLinkedList singlyLinkedList) {
+        return null;
+    }
+
+
+//    private Node head;
+//
+//    public class Node{
+//        private int data;
+//        private Node next;
+//
+//        public Node(int data) {
+//            this.data = data;
+//            this.next = null;
+//        }
+//    }
+//
+//    public void add(int data) {
+//        Node newNode = new Node(data);
+//        newNode.next = head;
+//        head = newNode;
+//    }
+//
+//    public void remove(int data) {
+//        Node currentNode = head;
+//        Node previousNode = null;
+//
+//        while (currentNode != null && currentNode.data != data) {
+//            previousNode = currentNode;
+//            currentNode = currentNode.next;
+//        }
+//
+//        if (currentNode != null) {
+//            if (previousNode == null) {
+//                head = currentNode.next;
+//            } else {
+//                previousNode.next = currentNode.next;
+//            }
+//        }
+//    }
+//
+//    public int find(int data) {
+//        Node currentNode = head;
+//
+//        while (currentNode != null && currentNode.data != data) {
+//            currentNode = currentNode.next;
+//        }
+//
+//        if (currentNode != null) {
+//            return currentNode.data;
+//        } else {
+//            return -1;
+//        }
+//    }
+//
+//    public boolean contains(int data){
+//        Node currentNode = head;
+//        return
+//    }
+
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,92 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testAddMethod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value = 10;
+        singlyLinkedList.add(value);
+        boolean actual = singlyLinkedList.contains(value);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testRemoveMethod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value = 10;
+        singlyLinkedList.add(value);
+        singlyLinkedList.remove(value);
+        boolean actual = singlyLinkedList.contains(value);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void testFindMethod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value = 10;
+        singlyLinkedList.add(value);
+        Integer actual = singlyLinkedList.find(value);
+        Assert.assertEquals((Integer) 1,actual);
+    }
+
+    @Test
+    public void testSizeMethod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value = 10;
+        singlyLinkedList.add(value);
+        Integer expected = 1;
+        Integer actual = singlyLinkedList.size();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testgetMethod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value = 10;
+        singlyLinkedList.add(value);
+        Integer actual = singlyLinkedList.get(value);
+        Integer expected = 10;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testCopyMehtod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value1 = 10;
+        Integer value2 = 20;
+        Integer value3 = 30;
+        singlyLinkedList.add(value1);
+        singlyLinkedList.add(value2);
+        singlyLinkedList.add(value3);
+        List<Integer> expected = new ArrayList<>(List.of( 10,20,30));
+        List<Integer> actual = singlyLinkedList.copy(singlyLinkedList);
+
+    }
+
+    @Test
+    public void testSortMehtod() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Integer value1 = 20;
+        Integer value2 = 10;
+        Integer value3 = 30;
+        singlyLinkedList.add(value1);
+        singlyLinkedList.add(value2);
+        singlyLinkedList.add(value3);
+        List<Integer> expected = new ArrayList<>(List.of( 10,20,30));
+        List<Integer> actual = singlyLinkedList.sort(singlyLinkedList);
+
+    }
+
+
 }
